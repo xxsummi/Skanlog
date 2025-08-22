1c37a885a3e6f4056afa5d8e3ce765fd1e8dda27
Jeskha Samantha Derama <jeskhasamanthaderama@gmail.com>
2025-08-22 10:45:32 +0800

    Exercise 1 Student Management

---
+++ Exercise1/Exercise1/Person.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Operations
+    {
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string School { get; set; }
+            public int SuspensionCount { get; set; }
+
+            public Person(int id, string name, int age, string school) 
+            {
+                Id = id;
+                Name = name;
+                Age = age;
+                School = school;
+                SuspensionCount = 0;
+            }
+
+            public override string ToString()
+            {
+                return $"Student ID: {Id}, Name: {Name}, Age: {Age}, School: {School}, Suspension Count: {SuspensionCount}";
+            }
+
+        }
+
+        public class PersonsList
+        {
+            public List<Person> List { get; set; }
+            public List<Person> Suspended { get; set; }
+
+            public PersonsList()
+            {
+                this.List = new List<Person>();
+                this.Suspended = new List<Person>();
+            }
+
+            public void AddPerson(int id, string name, int age, string school)
+            {
+                Person person = new Person(id, name, age, school);
+                this.List.Add(person);
+
+                Console.WriteLine("\nUpdated Lists after Adding Person: ");
+                displayLists();
+            }
+
+            public void SuspendPerson(int id)
+            {
+                if (List.Count == 0)
+                {
+                    Console.WriteLine("There is currently no student in the list.");
+                    return;
+                }
+                bool found = false;
+                for(int i=0; i<List.Count; ++i)
+                {
+                    if (List[i].Id == id)
+                    {
+                        found = true;
+                        List[i].SuspensionCount++;
+                        Suspended.Add(List[i]);
+                        this.List.RemoveAt(i);
+                        Console.WriteLine($"Successfully Suspended Studen.");
+                    }
+                }
+                if (found)
+                {
+                    Console.WriteLine("\nUpdated Lists after Unsuspending Person: ");
+                    displayLists();
+                }
+                else
+                {
+                    Console.WriteLine("Student not found.");
+                }
+            }
+
+            public void UnsuspendPerson(int id)
+            {
+                if(Suspended.Count == 0)
+                {
+                    Console.WriteLine("There is currently no suspended student.");
+                    return;
+                }
+                bool found = false;
+                for (int i = 0; i < Suspended.Count; ++i)
+                {
+                    if (Suspended[i].Id == id)
+                    {
+                        found=true;
+                        List.Add(Suspended[i]);
+                        this.Suspended.RemoveAt(i);
+                    }
+                }
+
+                if (found)
+                {
+                    Console.WriteLine("\nUpdated Lists after Unsuspending Person: ");
+                    displayLists();
+                }
+                else
+                {
+                    Console.WriteLine("Student not found.");
+                }
+
+                
+            }
+
+            public void displayLists()
+            {
+                if (List.Count() > 0)
+                {
+                    Console.WriteLine("List of Students: ");
+                    foreach (Person person in this.List)
+                    {
+                        Console.WriteLine(person.ToString());
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("There is currently no students in the list.");
+                }
+
+                if (Suspended.Count() > 0)
+                {
+                    Console.WriteLine("List of Suspended Students: ");
+                    foreach (Person person in this.Suspended)
+                    {
+                        Console.WriteLine(person.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("There is currently no suspended students.");
+                }
+                
+            }
+
+            public void displayNumbers()
+            {
+                Console.WriteLine($"List of Students: {List.Count()}");
+                Console.WriteLine($"List of Suspended Students: {Suspended.Count()} ");
+            }
+        }
+    }
+}
---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using Exercise1;
+
+int select, id;
+var StudentsList = new Operations.PersonsList();
+
+do
+{
+    Console.WriteLine("Student Management");
+    Console.WriteLine("1. Add a Student");
+    Console.WriteLine("2. Suspend a Student ");
+    Console.WriteLine("3. Unsuspend a Student ");
+    Console.WriteLine("4. Display List of Students and List of Suspended Students");
+    Console.WriteLine("5. Display Number of Students and Suspended Students");
+    Console.WriteLine("6. Exit Program");
+    Console.Write("Select Option: ");
+
+    if (int.TryParse(Console.ReadLine(), out select))
+    {
+        switch (select)
+        {
+            case 1:
+                Console.Write("Enter Student ID: ");
+                int.TryParse(Console.ReadLine(), out id);
+                Console.Write("Enter Student Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter age: ");
+                int.TryParse(Console.ReadLine(), out int age);
+                Console.Write("Enter School: ");
+                string school = Console.ReadLine();
+
+                StudentsList.AddPerson(id, name, age, school);
+                break;
+            case 2:
+                Console.WriteLine("Suspend a Student: ");
+                Console.Write("Enter student ID to suspend: ");
+                int.TryParse(Console.ReadLine(), out id);
+                StudentsList.SuspendPerson(id);
+                break;
+            case 3:
+                Console.WriteLine("Unsuspend a Student: ");
+                Console.Write("Enter student ID to unsuspend:");
+                int.TryParse(Console.ReadLine(), out id);
+                StudentsList.UnsuspendPerson(id);
+                break;
+            case 4:
+                StudentsList.displayLists();
+                break;
+            case 5:
+                StudentsList.displayNumbers();
+                break;
+            case 6:
+                Console.WriteLine("Exiting Program.");
+                break;
+            default:
+                Console.WriteLine("Invalid Input.");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid number.");
+    }
+    Console.WriteLine();
+} while (select != 6);
